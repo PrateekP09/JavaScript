6f594d3f16c82414cf5c7fa5a20c98f66bc4eb14
PrateekP09 <prateekgowda09@gmail.com>
2025-04-25 11:33:26 +0530

    js

---
+++ 7.array-methods/p1.js
@@ -0,0 +1,27 @@
+// instance methods
+        // storing and retriving values
+        // push(store) and pop(retrive)
+
+        let user=[];
+        console.log(`user=`,user)
+        //last in
+
+        user.push("john")
+        console.log(`user=`,user)
+
+        user.push("john")
+        console.log(`user=`,user)
+
+        user.push("john")
+        console.log(`user=`,user)
+
+        //last out
+        console.log(`pop=`, user.pop())
+        console.log(`user=`,user)
+
+        console.log(`pop=`, user.pop())
+        console.log(`user=`,user)
+        
+        console.log(`pop=`, user.pop())
+        console.log(`user=`,user)
+       
---
+++ 7.array-methods/p2.js
@@ -0,0 +1,20 @@
+var user=["Tom", "Ayan"];
+
+console.log(`user=`,user)
+
+//array.unshift (value)
+
+user.shift("Harry")// first in
+console.log(`after unshift=`,user)
+
+user.shift("Mark")// first in
+console.log(`after unshift=`,user)
+
+// array.shift()
+let x1= user.shift()// first out
+console.log(`after shift=`, user)
+console.log(`after shift=`, x1)
+
+let x2= user.shift()// first out
+console.log(`after shift=`, user)
+console.log(`after shift=`, x2)
